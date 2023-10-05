86aa1a8cab4638b868d59593906e35ebcd02c994
EnriqueIsra <sirilo99x3@gmail.com>
2023-10-05 11:27:16 -0600

    119. métodos arrays

--- js/14.js
+++ js/14.js
@@ -3,10 +3,9 @@
 const numeros = [10, 20, 30, 40, 50]
 console.table(numeros)
 
-const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo']
-console.log(meses)
 
-//acceder a los valores de un arreglo
+
+/* //acceder a los valores de un arreglo
 console.log(meses[4])
 console.log(numeros[4])
 
@@ -18,4 +17,23 @@ numeros.forEach(function(numero){
 })
 
 //conocer la extensión de un arreglo
-console.log(meses.length)
+console.log(meses.length) */
+
+numeros.push(60, 70, 80) // agregar deatos al final del arreglo
+numeros.unshift(-10, -20, -30) //agregar datos al inicio del arreglo
+console.table(numeros)
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo']
+
+/* meses.pop() //eliminar el valor final del arreglo
+meses.shift() // eliminar el primer valor del arreglo */
+/* meses.splice(2, 1)
+
+console.table(meses) */
+
+// Rest operator o Spread operator
+
+const nuevoArreglo = ['Diciembre', ...meses, 'Junio']
+console.log(nuevoArreglo)
+
+
